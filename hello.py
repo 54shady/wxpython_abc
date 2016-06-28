ffb055495e06c0d783ab57f6148b538d0bca9296
zeroway.lin <zeroway.lin@forelders.care>
2016-06-28 09:08:26 +0000

    father and child frame

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@
 import wx
 
 app = wx.App(False)
-frame = wx.Frame(None, wx.ID_ANY, "hello world")
-frame.Show(True)
+father = wx.Frame(None, wx.ID_ANY, "father_frame")
+child = wx.Frame(father, wx.ID_ANY, "child_frame")
+child.Show(True)
+father.Show(True)
 app.MainLoop()
