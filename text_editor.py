a7204689d96ee800489e257865becd3489ac5772
zeroway.lin <zeroway.lin@forelders.care>
2016-06-28 11:39:10 +0000

    add sizer

--- text_editor.py
+++ text_editor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import os
 import wx
 
 # MyFrame derive from wx.Frame and overwrite its __init__ method
 class MyFrame(wx.Frame):
     def __init__(self, title):
         wx.Frame.__init__(self, None, title=title, size=(200,100))
-        wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.myTextCtrl = wx.TextCtrl(self, style=wx.TE_MULTILINE)
 
         # statusbar in the bottom of the window
         self.CreateStatusBar()
@@ -29,27 +30,44 @@ class MyFrame(wx.Frame):
 
         self.SetMenuBar(menuBar)
 
+        # sizer
+        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+        self.buttons = []
+        for i in range(0, 6):
+            self.buttons.append(wx.Button(self, -1, "Button &"+str(i)))
+            self.sizer2.Add(self.buttons[i], 1, wx.EXPAND)
+
+        # Use some sizers to see layout options
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.myTextCtrl, 1, wx.EXPAND)
+        self.sizer.Add(self.sizer2, 0, wx.EXPAND)
+
+        #Layout sizers
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(1)
+        self.sizer.Fit(self)
+
         # show myself
         self.Show(True)
     
     def OnAbout(self, e):
-        dlg = wx.MessageDialog(self, "this is all about:)")
+        dlg = wx.MessageDialog(self, "aaaa", "bbbbb", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
 
     def OnExit(self, e):
-        dlg = wx.MessageDialog(self, "Exiiiiiiiiiiiiit")
+        dlg = wx.MessageDialog(self, "Exiiiit")
         dlg.ShowModal()
         dlg.Destroy()
 
-    def OnOpen(self,e):
+    def OnOpen(self, e):
         self.dirname = ''
         dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
             f = open(os.path.join(self.dirname, self.filename), 'r')
-            self.control.SetValue(f.read())
+            self.myTextCtrl.SetValue(f.read())
             f.close()
         dlg.Destroy()
 
