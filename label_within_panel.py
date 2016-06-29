47796da7d313c26be1f43301368e45c05db3bfff
zeroway <M_O_Bz@163.com>
2016-06-29 02:29:21 +0000

    panel add logger and button

--- label_within_panel.py
+++ label_within_panel.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 import wx
 
-# MyFrame id derive from wx Frame
-class MyFrame(wx.Frame):
-    def __init__(self, p):
-        wx.Frame.__init__(self, p)
-        panel = wx.Panel(self)
-        wx.StaticText(panel, label="You quote", pos=(20, 30))
-        self.Show()
+class ExamplePanle(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        wx.StaticText(self, label="You quote", pos=(20, 30))
+
+        self.logger = wx.TextCtrl(self, pos=(300, 20), size=(200, 300), style=wx.TE_MULTILINE | wx.TE_READONLY)
+
+        # button
+        self.button = wx.Button(self, label="save", pos=(200, 325))
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
+
+    def OnClick(self, event):
+        self.logger.AppendText("Click on the object with Id %d\n" % event.GetId())
 
 app = wx.App(False)
-MyFrame(None)
+frame = wx.Frame(None)
+panel = ExamplePanle(frame)
+frame.Show()
 app.MainLoop()
