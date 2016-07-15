6969583ba95489eaa2f8fc5c98ae50a4b5c92abb
zeroway <M_O_Bz@163.com>
2016-07-15 02:35:40 +0000

    add a quit menuitem to the menu

--- menu1.py
+++ menu1.py
@@ -17,6 +17,12 @@ class MyMenu(wx.Frame):
         # add some item to the menu
         file.Append(101, 'Open', 'Open a new document')
         file.Append(102, 'Save', 'Save the document')
+        file.AppendSeparator()
+
+        # make a quit item and add to file menu
+        quit = wx.MenuItem(file, 105, 'Quit', 'Quit the Application')
+        quit.SetBitmap(wx.Image('stock_exit_24.png', wx.BITMAP_TYPE_PNG).ConvertToBitmap())
+        file.AppendItem(quit)
 
         # add menus to the menubar
         menubar.Append(file, 'File')
@@ -30,6 +36,7 @@ class MyApp(wx.App):
     def OnInit(self):
         frame = MyMenu(None, wx.ID_ANY, "menu1.py")
         frame.SetIcon(wx.Icon('icon.ico', wx.BITMAP_TYPE_ICON))
+        frame.Center()
         frame.Show(True)
         return True
 
